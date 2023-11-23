f0b18d19b01d3e2004bfd51e686b964be4388e10
Katrien De Graeve <katriendg@users.noreply.github.com>
2023-11-23 09:05:51 +0100

    Devcontainer line fix

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -19,7 +19,7 @@ k3d registry create k3d-devregistry.localhost --port 5500
 
 # Create k3d cluster with NFS support and forwarded ports
 # See https://github.com/jlian/k3d-nfs
-k3d cluster create devcluster --registry-use k3d-devregistry.localhost:5500 -i ghcr.io/jlian/k3d-nfs:v1.25.3-k3s1
+k3d cluster create devcluster --registry-use k3d-devregistry.localhost:5500 -i ghcr.io/jlian/k3d-nfs:v1.25.3-k3s1 \
 -p '1883:1883@loadbalancer' \
 -p '8883:8883@loadbalancer' \
 -p '6001:6001@loadbalancer' \
