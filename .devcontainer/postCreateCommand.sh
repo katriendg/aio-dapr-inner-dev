01aa63123d866f0c504b18a45744353b85f2a442
Katrien De Graeve <katriendg@users.noreply.github.com>
2024-01-10 10:32:22 +0000

    some changes to devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -6,10 +6,6 @@ set -o pipefail
 
 echo "Starting Post Create Command"
 
-az extension add --name connectedk8s
-az extension add --name k8s-extension
-az extension add --name azure-iot-ops
-
 # This env var is important to allow k3s to support shared mounts, required for CSI driver
 # Temporary fix until made default https://github.com/k3d-io/k3d/pull/1268#issuecomment-1745466499
 export K3D_FIX_MOUNTS=1
@@ -29,8 +25,7 @@ if [[ $(k3d cluster list | grep devcluster) ]]; then
 else
     k3d cluster create devcluster --registry-use k3d-devregistry.localhost:5500 -i ghcr.io/jlian/k3d-nfs:v1.25.3-k3s1 \
     -p '1883:1883@loadbalancer' \
-    -p '8883:8883@loadbalancer' \
-    -p '5111:5111@loadbalancer'
+    -p '8883:8883@loadbalancer' 
 
     # Install Dapr on cluster    
     helm repo add dapr https://dapr.github.io/helm-charts/
